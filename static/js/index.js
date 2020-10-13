aa00c58853c5befc31387559026e719cdb0f56d2
Amansingh1202 <aks001235@gmail.com>
2020-10-13 15:21:09 +0530

    added typed js

---
+++ static/js/index.js
@@ -0,0 +1,8 @@
+var typed = new Typed('.change', {
+    strings: ["<span style='color:orange;font-family:sans-serif'>Crusty</span>", "<span style='color:lightblue;font-family:serif'>Savory</span>","<span style='color:yellow;font-family: Courier'>Juicy</span>"],
+    typeSpeed: 100,
+    loop: true,
+    loopCount: Infinity,
+    startDelay: 100,
+    backSpeed: 50,
+  });
