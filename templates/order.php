fae11aa6fb665055f35d9d9d86f16e6ee593a75b
Amansingh1202 <aks001235@gmail.com>
2020-10-14 15:11:40 +0530

    completed order page

--- templates/order.php
+++ templates/order.php
@@ -5,7 +5,9 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Order Food</title>
     <link rel="stylesheet" type="text/css" href="../static/css/style.css">
-</head>
+    <link rel="stylesheet" type="text/css" href="../static/css/order.css">
+
+  </head>
 <body>
     <header>
         <div class="container nav1">
@@ -21,20 +23,92 @@
          </header>
 
 <div>
+  <h3> Order your food now,we will deliver it to your doorstep!</h3>
+<form action="../src/confirm.php" method="post">
 <?php
 $mysqli = new mysqli("localhost","root","","restaurant");
 $result = $mysqli -> query("SELECT * FROM food");
+echo "<table>";
+echo "<tr>";
+echo "<td class='clr'>Select Food 1:</td>";
+echo "<td>";
+echo "<select name='food1' class='food'>";
+echo "<option value='none'>Not Selected</option>";
 if ($result) {
   while($row = $result->fetch_assoc()) {
-    echo $row['name'];
+    echo "<option value=".$row['food_id']."  >".$row['name']." ,Price:".$row['price']."</option>";
   }
 
 }
 else {
   echo mysql_error();
 }
+echo "</select>";
+echo "</td>";
+echo "</tr>";
+
+$result1 = $mysqli -> query("SELECT * FROM food");
+echo "<tr>";
+echo "<td class='clr'>Select Food 2:</td>";
+echo "<td>";
+echo "<select name='food2' class='food'>";
+echo "<option value='none'>Not Selected</option>";
+if ($result1) {
+  while($row1 = $result1->fetch_assoc()) {
+    echo "<option value=".$row1['food_id']."  >".$row1['name']." ,Price:".$row1['price']."</option>";
+  }
+
+}
+else {
+  echo mysql_error();
+}
+echo "</select>";
+echo "</td>";
+echo "</tr>";
+
+$result2 = $mysqli -> query("SELECT * FROM food");
+echo "<tr>";
+echo "<td class='clr'>Select Food 3:</td>";
+echo "<td>";
+echo "<select name='food3' class='food'>";
+echo "<option value='none'>Not Selected</option>";
+if ($result2) {
+  while($row2 = $result2->fetch_assoc()) {
+    echo "<option value=".$row2['food_id']."  >".$row2['name']." ,Price:".$row2['price']."</option>";
+  }
+
+}
+else {
+  echo mysql_error();
+}
+echo "</select>";
+echo "</td>";
+echo "</tr>";
+
+$result3 = $mysqli -> query("SELECT * FROM food");
+echo "<tr>";
+echo "<td class='clr'>Select Food 4:</td>";
+echo "<td>";
+echo "<select name='food4' class='food'>";
+echo "<option value='none'>Not Selected</option>";
+if ($result3) {
+  while($row3 = $result3->fetch_assoc()) {
+    echo "<option value=".$row3['food_id']."  >".$row3['name']." ,Price:".$row3['price']."</option>";
+  }
+
+}
+else {
+  echo mysql_error();
+}
+echo "</select>";
+echo "</td>";
+echo "</tr>";
+
+echo"</table>";
 $mysqli -> close();
-?> 
+?>
+<button type="submit">Submit</button>
+</form>
 </div>
 
          <footer class="main-footer">
